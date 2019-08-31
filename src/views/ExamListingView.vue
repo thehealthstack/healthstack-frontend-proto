e79a1e73d3af8665ad772019d06abd8a652432cf
vladimirfomene <vladimirfomene@gmail.com>
2019-08-31 12:16:01 +0100

    exam listing and exam request listing

---
+++ src/views/ExamListingView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <navbar-component></navbar-component>
+        <div class="columns">
+            <sidenavbar-component></sidenavbar-component>
+            <div class="column">
+                <div class="level">
+                    <examsearch-component></examsearch-component>
+                    <createnewexam-component></createnewexam-component>
+                    <createnewvaccin-component></createnewvaccin-component>
+                </div>
+                <examlistingtable-component></examlistingtable-component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import examSearchComponent from "@/components/ExamSearchComponent.vue";
+import createNewExamComponent from "@/components/CreateNewExamComponent.vue";
+import createNewVaccinComponent from "@/components/CreateNewVaccinComponent.vue";
+import examListingRowComponent from "@/components/ExamListingRowComponent.vue";
+import sideNavBarComponent from "@/components/SideNavBarComponent.vue";
+import searchBarComponent from "@/components/SearchBarComponent.vue";
+import examListingTableComponent from "@/components/ExamListingTableComponent.vue";
+import navBarComponent from "@/components/NavBarComponent.vue";
+
+export default {
+    name: "ExamListingView",
+    components: {
+        "examsearch-component": examSearchComponent,
+        "createnewexam-component": createNewExamComponent,
+        "createnewvaccin-component": createNewVaccinComponent,
+        "examlistingrow-component": examListingRowComponent,
+        "sidenavbar-component": sideNavBarComponent,
+        "searchbar-component": searchBarComponent,
+        "examlistingtable-component": examListingTableComponent,
+        "navbar-component": navBarComponent,
+    }
+};
+</script>
---
+++ src/views/ExamRequestListingView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <navbar-component></navbar-component>
+        <div class="container">
+            <div class="columns">
+                <sidenavbar-component></sidenavbar-component>
+                <div class="column">
+                    <searchbar-component></searchbar-component>
+                    <examrequesttable-component></examrequesttable-component>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import sideNavBarComponent from "@/components/SideNavBarComponent.vue";
+import examRequestsTableComponent from "@/components/ExamRequestsTableComponent.vue";
+import searchBarComponent from "@/components/SearchBarComponent.vue";
+import navBarComponent from "@/components/NavBarComponent.vue";
+
+export default {
+    name: "ExamRequestListingView",
+    components: {
+    "sidenavbar-component": sideNavBarComponent,
+    "examrequesttable-component": examRequestsTableComponent,
+    "searchbar-component": searchBarComponent,
+    "navbar-component": navBarComponent
+  }
+}
+</script>
