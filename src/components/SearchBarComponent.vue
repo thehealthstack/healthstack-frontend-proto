e87845841c95801fbfc2bb196e86a551565a5304
vladimirfomene <vladimirfomene@gmail.com>
2019-08-31 12:14:39 +0100

    create patient search bar

---
+++ src/components/SearchBarComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="section">
+    <div class="field has-addons">
+      <div class="control">
+        <input class="input" type="text" placeholder="Find a patient file" />
+      </div>
+      <select class="is-info">
+        <option selected>Search By</option>
+        <option>Patient File</option>
+        <option>Patient Name</option>
+      </select>
+      <div class="control">
+        <a class="button is-link">Search</a>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/SearchPatientFileComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="field">
+        <div class="control">
+            <input class="input is-link" type="text" placeholder="Search Patient file">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchPatientFileComponent"
+}
+</script>
