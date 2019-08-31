b8d70256ed7b0d49db3eb23685096210a9c5a0fd
vladimirfomene <vladimirfomene@gmail.com>
2019-08-31 12:08:41 +0100

    create new vaccin component

---
+++ src/components/CreateNewVaccinComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <a class="button is-link" @click="activateModal">Create Vaccin</a>
+        <newvaccin-component @close="closeModal" :isActive="isActive"></newvaccin-component>
+    </div>
+</template>
+
+<script>
+import newVaccinComponent from "@/components/NewVaccinComponent.vue";
+
+export default {
+    name: "CreateNewVaccinComponent",
+    components: {
+        "newvaccin-component": newVaccinComponent,
+    },
+    methods: {
+        activateModal(evt){
+            this.isActive = true;
+        },
+        closeModal(evt){
+            this.isActive = false;
+        }
+    },
+    data(){
+        return {
+            isActive: false,
+        }
+    }
+}
+</script>
