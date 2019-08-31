b65332fa416af81d6f07d62c4c919634f3c58e58
vladimirfomene <vladimirfomene@gmail.com>
2019-08-31 12:13:57 +0100

    create navbars

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="level navbar is-light is-marginless">
+        <div class="level-left">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="index.html">
+                <strong class="brand">HealthStack</strong>
+                </a>
+            </div>
+        </div>
+        <div class="level-right">
+            <div class="navbar-end">
+            <div class="navbar-item has-dropdown is-hoverable">
+                <div class="navbar-link">Vladimir Kwasi</div>
+                <div class="navbar-dropdown">
+                </div>
+            </div>
+            </div>
+        </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "NavBarComponent"
+}
+</script>
---
+++ src/components/SideNavBarComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="column is-4-tablet is-3-desktop is-2-widescreen" style="margin-top: 4%">
+        <nav class="menu">
+            <ul class="menu-list">
+                <li>
+                    <a class="is-active" href="">
+                        <span class="icon">
+                            <i class="fa fa-book"></i>
+                        </span>
+                    Patient Exams
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span class="icon">
+                            <i class="fa fa-book"></i>
+                        </span>
+                    Exam Listing
+                    </a>
+                </li>
+            </ul>
+        </nav>
+  </div>
+</template>
