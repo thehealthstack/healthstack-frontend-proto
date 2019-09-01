3ee2f78ef1c4b10f0d2bf10e0d028094c6383652
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2019-09-01 16:35:54 +0300

    Update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,132 +22,79 @@
       <div id="navbar" class="navbar-menu">
         <div class="navbar-end">
           <div class="navbar-item">
-            <div class="field is-grouped is-grouped-multiline">
-              <div class="control has-icons-left is-expanded">
-                <input class="input" type="email" placeholder="Email" />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-envelope"></i>
-                </span>
-              </div>
-              <div class="control has-icons-left is-expanded">
-                <input class="input" type="password" placeholder="Password" />
-                <span class="icon is-small is-left">
-                  <i class="fas fa-lock"></i>
-                </span>
-              </div>
-              <div class="control is-expanded">
-                <button class="button is-link">Login</button>
-              </div>
+            <div class="field is-grouped">
+              <p class="control">
+                <a class="button">
+                  <span class="icon">
+                    <i class="fas fa-users"></i>
+                  </span>
+                  <span>
+                    Log in
+                  </span>
+                </a>
+              </p>
+              <p class="control">
+                <a class="button is-link" href="#">
+                  <span>Sign up</span>
+                </a>
+              </p>
             </div>
           </div>
         </div>
       </div>
     </nav>
-    <section class="hero">
+    <section class="hero is-medium">
       <div class="hero-body">
-        <div class>
-          <div class="level signup-section">
-            <div class="level-left">
-              <p class="text has-text-white is-size-3">
-                We help you optimize your workflow,
-                <br />improve your customer experience
-                <br />and save
-              </p>
-            </div>
-            <div class="level-right">
-              <form class="box">
-                <div class="field is-grouped is-grouped-multiline">
-                  <div class="control is-expanded">
-                    <input class="input" type="text" placeholder="First Name" required />
-                  </div>
-                  <div class="control is-expanded">
-                    <input class="input" type="text" placeholder="Last Name" required />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <input class="input" type="email" placeholder="Email" required />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <input
-                      class="input"
-                      type="tel"
-                      placeholder="Phone Number with Country Code"
-                      required
-                    />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <div class="select is-fullwidth">
-                      <select>
-                        <option>Lab Administrator</option>
-                        <option>Cashier</option>
-                        <option>Receptionist</option>
-                        <option>Clinical Pathologist</option>
-                        <option>lab technician</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <input class="input" type="password" placeholder="Password" required />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <input
-                      class="input"
-                      type="password"
-                      placeholder="Password Confirmation"
-                      required
-                    />
-                  </div>
-                </div>
-                <div class="field">
-                  <div class="control">
-                    <button class="button is-link">Signup</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
+        <div class="container">
+          <h1 class="title has-text-white">
+            We help you optimize your workflow, improve your customer experience and save
+          </h1>
+          <h2 class="subtitle has-text-white">
+            Medium subtitle
+          </h2>
         </div>
       </div>
     </section>
-    <div class="columns is-vcentered">
-      <div class="column">
-        <h1 class="title has-text-centered">Workflow Management</h1>
-        <p class="has-text-centered">
-          This service allows you to accept electronic payment from your
-          customers whatever payment method they are using.
-        </p>
+    <section class="section">
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <h1 class="title">Workflow Management</h1>
+            <p class="subtitle">
+              This service allows you to accept electronic payment from your
+              customers whatever payment method they are using.
+            </p>
+          </article>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <h1 class="title">Patient Data Management</h1>
+            <p class="subtitle">
+              This service allows you to accept electronic payment from your
+              customers whatever payment method they are using.
+            </p>
+          </article>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <h1 class="title">Billing Management</h1>
+            <p class="subtitle">
+              This service allows you to accept electronic payment from your
+              customers whatever payment method they are using.
+            </p>
+          </article>
+        </div>
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <h1 class="title">Medical Result Notification & Delivery</h1>
+            <p class="subtitle">
+              This service allows you to accept electronic payment from your
+              customers whatever payment method they are using.
+            </p>
+          </article>
+        </div>
       </div>
-      <div class="column">
-        <h1 class="title has-text-centered">Patient Data Management</h1>
-        <p class="has-text-centered">
-          This service allows you to accept electronic payment from your
-          customers whatever payment method they are using.
-        </p>
-      </div>
-      <div class="column">
-        <h1 class="title has-text-centered">Billing Management</h1>
-        <p class="has-text-centered">
-          This service will give you the tools to schedule your customers whenever
-          they come for a medical examination.
-        </p>
-      </div>
-      <div class="column">
-        <h1 class="title has-text-centered">Medical Result Notification & Delivery</h1>
-        <p class="has-text-centered">
-          This service allows you to use Sms and Email to send your
-          customers' medical exam results once they are ready.
-        </p>
-      </div>
-    </div>
+    </section>
     <footer class="footer has-background-black-ter">
       <div class="columns">
         <div class="column">
