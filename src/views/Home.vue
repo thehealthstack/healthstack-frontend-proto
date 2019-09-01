16b4c8425f4b84ec804619572f6a195405dc0728
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2019-09-01 18:04:08 +0300

    Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -90,31 +90,6 @@
         <div class="column">
           <h1 class="is-vcentered footer-brand">HealthStack</h1>
         </div>
-        <div class="column">
-          <h3>Products</h3>
-          <ul>
-            <li>Delivery</li>
-            <li>Payment</li>
-            <li>Scheduling</li>
-          </ul>
-        </div>
-        <div class="column">
-          <h3>Company</h3>
-          <ul>
-            <li>About</li>
-            <li>Team</li>
-            <li>Careers</li>
-            <li>Customers</li>
-          </ul>
-        </div>
-        <div class="column">
-          <h3>Resource</h3>
-          <ul>
-            <li>Contact</li>
-            <li>Support</li>
-            <li>Terms</li>
-          </ul>
-        </div>
       </div>
     </footer>
   </div>
