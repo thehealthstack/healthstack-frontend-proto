52069a7695f1336f3c9f2f53eee5e16db173ebaf
vladimirfomene <vladimirfomene@gmail.com>
2019-09-02 18:19:52 +0100

    select Action dropdow to act on selection

--- src/components/ExamRequestsTableRowComponent.vue
+++ src/components/ExamRequestsTableRowComponent.vue
@@ -1,42 +1,68 @@
 <template>
-    <tr>
-        <td>{{ examRequest.examRequestId }}</td>
-        <td>{{ examRequest.patient.firstName + " " + examRequest.patient.lastName }}</td>
-        <td>{{ examRequest.patient.age }}</td>
-        <td>{{ examRequest.patient.sex }}</td>
-        <td>{{ examRequest.patient.email }}</td>
-        <td>{{ examRequest.patient.telephone }}</td>
-        <td>{{ examRequest.doctor.name }}</td>
-        <td>{{ examRequest.status }}</td>
-        <td>
-            <select class="select">
-                <option>Select Action</option>
-                <option @click="callVerificationModal" v-show="session.user.role=='biologist'">Verify Results</option>
-                <option>Specimen Collected</option>
-                <option @click="callRegistrationModal">Register Results</option>
-            </select>
-        </td>
-        <resultverification-component @close="closeVerificationModal" :isActive="isActiveVerification" :examRequest="examRequest"></resultverification-component>
-        <resultregistration-component @closeregistration="closeRegistrationModal" :isActive="isActiveRegistration" :examRequest="examRequest"></resultregistration-component>
-    </tr>
+  <tr>
+    <td>{{ examRequest.examRequestId }}</td>
+    <td>{{ examRequest.patient.firstName + " " + examRequest.patient.lastName }}</td>
+    <td>{{ examRequest.patient.age }}</td>
+    <td>{{ examRequest.patient.sex }}</td>
+    <td>{{ examRequest.patient.email }}</td>
+    <td>{{ examRequest.patient.telephone }}</td>
+    <td>{{ examRequest.doctor.name }}</td>
+    <td>{{ examRequest.status }}</td>
+    <td>
+      <select class="select" @change="callAction" v-model="selection">
+        <option>Select Action</option>
+        <option>Verify Results</option>
+        <option>Specimen Collected</option>
+        <option>Register Results</option>
+      </select>
+    </td>
+    <resultverification-component
+      @close="closeVerificationModal"
+      :isActive="isActiveVerification"
+      :examRequest="examRequest"
+    ></resultverification-component>
+    <resultregistration-component
+      @closeregistration="closeRegistrationModal"
+      :isActive="isActiveRegistration"
+      :examRequest="examRequest"
+    ></resultregistration-component>
+  </tr>
 </template>
 
 <script>
 import resultVerificationComponent from "@/components/ResultVerificationComponent";
 import resultRegisterationComponent from "@/components/ResultRegistrationComponent";
+import { editExamRequestById } from "@/database/index.js";
 
 export default {
     name: "ExamRequestsTableRowComponent",
     props: ["examRequest", "session"],
     data(){
         return {
+            selection: "Select Action",
             isActiveVerification: false,
             isActiveRegistration: false,
         }
     },
     methods: {
-        callVerificationModal(){
-            this.isActiveVerification = true;
+        async callAction(){
+            if(this.selection == "Verify Results"){
+                this.isActiveVerification = true;
+            }
+
+            if(this.selection == "Register Results"){
+                this.isActiveRegistration = true;
+            }
+
+            if(this.selection == "Specimen Collected"){
+                this.examRequest.status = "Specimen Collected";
+                try{
+                    let examRequest = await editExamRequestById(this.examRequest);
+                } catch(err){
+                    console.error(err);
+                }
+            }
+            
         },
         closeVerificationModal(){
             this.isActiveVerification = false;
@@ -44,9 +70,6 @@ export default {
         closeRegistrationModal(){
             this.isActiveRegistration = false;
         },
-        callRegistrationModal(){
-            this.isActiveRegistration = true;
-        }
     },
     components: {
         "resultverification-component": resultVerificationComponent,
