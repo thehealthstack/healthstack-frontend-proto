1cd9d07393122cf7bc0e005603025b28cd9555bc
vladimirfomene <vladimirfomene@gmail.com>
2019-09-02 18:24:40 +0100

    add router-link to sideNavBar

--- src/components/SideNavBarComponent.vue
+++ src/components/SideNavBarComponent.vue
@@ -1,24 +1,27 @@
 <template>
-    <div class="column is-4-tablet is-3-desktop is-2-widescreen" style="margin-top: 4%">
-        <nav class="menu">
-            <ul class="menu-list">
-                <li>
-                    <a class="is-active" href="">
-                        <span class="icon">
-                            <i class="fa fa-book"></i>
-                        </span>
-                    Patient Exams
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        <span class="icon">
-                            <i class="fa fa-book"></i>
-                        </span>
-                    Exam Listing
-                    </a>
-                </li>
-            </ul>
-        </nav>
+  <div class="column is-4-tablet is-3-desktop is-2-widescreen" style="margin-top: 4%">
+    <nav class="menu">
+      <ul class="menu-list">
+        <li>
+          <router-link :to="{ path: '/:laboratoryname/labtech/:username'}" exact-active-class="is-active">
+            <span class="icon">
+              <i class="fa fa-book"></i>
+            </span>
+            Exam Requests
+          </router-link>
+        </li>
+        <li>
+          <router-link
+            :to="{ path: '/:laboratoryname/labtech/:username/exams'}"
+            exact-active-class="is-active"
+          >
+            <span class="icon">
+              <i class="fa fa-book"></i>
+            </span>
+            Exam Listing
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
