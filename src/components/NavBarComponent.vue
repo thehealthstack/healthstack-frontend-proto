a1fa6d24c737cf5c472ba75e7e5ce0a321c0d048
vladimirfomene <vladimirfomene@gmail.com>
2019-09-03 12:48:13 +0100

    add navigation to the application

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -6,6 +6,34 @@
                 <strong class="brand">HealthStack</strong>
                 </a>
             </div>
+            <div class="navbar-brand">
+                <router-link to="/pasteur/receptionist/totopasteur/registration">
+                    <span class="navbar-item">
+                        <strong class="brand">Receptionist</strong>
+                    </span>
+                </router-link>
+            </div>
+            <div class="navbar-brand">
+                <router-link to="/pasteur/cashier/totopasteur">
+                    <span class="navbar-item">
+                        <strong class="brand">Cashier</strong>
+                    </span>
+                </router-link>
+            </div>
+            <div class="navbar-brand">
+                <router-link to="/pasteur/labtech/totopasteur">
+                    <span class="navbar-item">
+                        <strong class="brand">Lab Tech</strong>
+                    </span>
+                </router-link>
+            </div>
+            <div class="navbar-brand">
+                <router-link to="/pasteur/biologist/totopasteur">
+                    <span class="navbar-item">
+                        <strong class="brand">Biologist</strong>
+                    </span>
+                </router-link>
+            </div>
         </div>
         <div class="level-right">
             <div class="navbar-end">
